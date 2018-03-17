357ff1f160890ffa2f9ec890444650ac864f01db
Imran Khan <imrankhan.in3@gmail.com>
2018-03-17 18:29:01 +0530

    [master]:Steps to insert testJar.jar

--- src/main/java/com/mypack/Test1.java
+++ src/main/java/com/mypack/Test1.java
@@ -3,7 +3,12 @@ import org.apache.log4j.Logger;
 
 import com.mypack.TestJar;
 
-
+/**
+ * clone project https://github.com/ik21191/testJar.git using git bash eg. git clone https://github.com/ik21191/testJar.git
+ * build the project mvn clean package
+ * add testJar.jar to this project using build path
+ * This project was created to demonstrate the use of slf4j
+ * */
 public class Test1 {
 	private static final Logger log = Logger.getLogger(Test1.class);
     public static void main(String[] args) {
