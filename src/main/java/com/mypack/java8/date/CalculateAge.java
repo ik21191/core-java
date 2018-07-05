7e6dcecabc41f46fc8a3fdf2e4161160d9202374
Imran Khan <imrankhan.in3@gmail.com>
2018-07-05 16:09:11 +0530

    [regex]:Some changes regarding regex

--- src/main/java/com/mypack/java8/date/CalculateAge.java
+++ src/main/java/com/mypack/java8/date/CalculateAge.java
@@ -4,8 +4,12 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.Month;
 import java.time.Period;
+import java.time.YearMonth;
 import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 import org.apache.log4j.Logger;
 
@@ -18,9 +22,24 @@ public class CalculateAge {
 		log.info("Year: " + p.getYears() + ", Month: " + p.getMonths() + ", Day: " + p.getDays());
 		
 		String dateTime = LocalDateTime.now(ZoneId.systemDefault()).format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+		log.info(dateTime);
+		YearMonth yearMonth = YearMonth.of(2018, 7);
+		log.info(yearMonth.toString());
+		
+		List<Integer> list = new ArrayList<>();
+		
+		list.add(10);
+		list.add(2);
+		list.add(90);
+		list.add(8);
+		list.add(1);
+		
+		Collections.sort(list);
+		log.info(list);
+		log.info(list.get(0));
+		log.info(list.get(list.size()-1));
 		
 		
-		System.out.println(dateTime);
+		//log.info(Integer.parseInt("01"));
 	}
-
 }
--- src/main/java/com/mypack/regex/SplitAndRegex.java
+++ src/main/java/com/mypack/regex/SplitAndRegex.java
@@ -8,6 +8,13 @@ public class SplitAndRegex {
 	public static void main(String arr[]) {
 		String name = "https://ieeexplore.ieee.org/stamp/stamp.jsp?tp=&arnumber=4028013";
 		log.info(name.split("\\?")[1]);
+		
+		String name2 = "/stamp/stamp.jsp";
+		log.info(name2.contains("/stamp/stamp.jsp"));
+		
+		String stringWithNumber = "1855";
+		
+		System.out.println("number check: " + stringWithNumber.matches("[0-9]+"));
 	}
 	
 }
