573ea0eda6a02e938689c3bc18cb631f9db28b8a
Imran Khan <imrankhan.in3@gmail.com>
2022-01-27 20:07:04 +0530

    [3.0-SNAPSHOT]: DB properties changes.

--- basic/src/main/java/com/mypack/db/OracleTest.java
+++ basic/src/main/java/com/mypack/db/OracleTest.java
@@ -14,20 +14,16 @@ public class OracleTest {
 			Class.forName("oracle.jdbc.driver.OracleDriver");
 
 			// step2 create the connection object
-			Connection con = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1522:XE", "SYSTEM", "123456");
+			Connection con = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1522/XEPDB1", "root", "root1");
 
 			// step3 create the statement object
 			Statement stmt = con.createStatement();
 
 			// step4 execute query
-			ResultSet rs = stmt.executeQuery("select * from test");
+			ResultSet rs = stmt.executeQuery("SELECT * FROM DUAL");
 			while (rs.next()) {
-				//long roll = rs.getLong(2); // To check whether the roll is null we need to call getString method.
-				String roll = rs.getString(2);
-				System.out.println("Name: " + rs.getString(1));
-				System.out.println("Roll: " + roll);
+				System.out.println("Column value is: " + rs.getString(1));
 			}
-
 			// step5 close the connection object
 			con.close();
 			System.out.println("End");
