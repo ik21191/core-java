201f0d347b38427a3d338191916c42c9343e81ce
Imran Khan <imrankhan.in3@gmail.com>
2025-12-23 11:55:33 +0530

    [upgrade_jdk_from_17_to_21]: Upgrading jdk version from 17 to 21

--- threading/src/main/java/com/mypack/thread/concurrent/executor/CallableWithExceptionDemo.java
+++ threading/src/main/java/com/mypack/thread/concurrent/executor/CallableWithExceptionDemo.java
@@ -52,7 +52,7 @@ public class CallableWithExceptionDemo {
 		for(Future<Integer> future : futureList) {
 			try {
 				/**
-				 * If we don't make the call to the get() method of Future class – then the exception thrown by call() 
+				 * If we don't make the call to the get() method of Future class - then the exception thrown by call() 
 				 * method will not be reported back, and the task will still be marked as completed*/
 				int result = future.get();
 				System.out.println(result);
